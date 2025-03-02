c5bd0ac9e73b0487d0b29305752c8e411588600e
Narendrabgus <111202314983@mhs.dinus.ac.id>
2025-03-02 14:07:25 +0700

    Add files via upload

---
+++ PreTest.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Document</title>
+  </head>
+  <body>
+    <form action="POST">
+      <div>
+        NIM : <input type="username" />
+        <br />
+        NAMA : <input type="text" />
+      </div>
+      <input type="button" value="simpan" />
+    </form>
+
+    <?php
+      
+    ?>
+  </body>
+</html>
