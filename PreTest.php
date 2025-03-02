3eae5618ae0586a60c36d394cf9ac2fd4669ef73
Narendrabgus <1112314983@mhs.dinus.ac.id>
2025-03-02 14:42:12 +0700

    update

--- PreTest.php
+++ PreTest.php
@@ -4,17 +4,23 @@
     <title>Document</title>
   </head>
   <body>
-    <form action="POST">
+    <form action="" method="POST">
       <div>
-        NIM : <input type="username" />
+        NIM : <input type="text" name="nim"/>
         <br />
-        NAMA : <input type="text" />
+        NAMA : <input type="text" name="nama"/>
       </div>
-      <input type="button" value="simpan" />
+      <input type="submit" name="simpan" value="simpan" />
     </form>
 
-    <?php
-      
-    ?>
+    <?php 
+if (isset($_POST['simpan'])) {
+    $nim = $_POST['nim'];
+    $nama = $_POST['nama']; 
+    
+    echo "Hello $nim, $nama";
+}
+?>
+
   </body>
 </html>
